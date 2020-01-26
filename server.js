208a5561ce10f71c422e337766e43d499466f874
Felix Fernandez <fernandezfelix460.gmail.com>
2020-01-26 11:20:17 -0500

    created folders and articles and scrape routes

---
+++ server.js
@@ -0,0 +1,84 @@
+
+
+var express = require("express");
+
+var mongoose = require("mongoose");
+
+// Our scraping tools
+// Axios is a promised-based http library, similar to jQuery's Ajax method
+// It works on the client and on the server
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+// Require all models
+var db = require("./models");
+
+var PORT = 3000;
+
+// Initialize Express
+var app = express();
+
+// Configure middleware
+
+// Use morgan logger for logging requests
+
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Make public a static folder
+app.use(express.static("public"));
+
+// Connect to the Mongo DB
+mongoose.connect("mongodb://localhost/mongoHeadlines", { useNewUrlParser: true });
+
+// A GET route for scraping the echoJS website
+app.get("/scrape", function(req, res) {
+    // First, we grab the body of the html with axios
+    axios.get("http://newjersey.news12.com/").then(function(response) {
+      // Then, we load that into cheerio and save it to $ for a shorthand selector
+      var $ = cheerio.load(response.data);
+
+      $(".headline").each(function(i, element) {
+        // Save an empty result object
+        var result = {};
+
+        result.headline = $(this).text();
+        result.url = $(this).children("a");
+        
+        
+        console.log(result);
+        db.Article.create(result)
+        .then(function(dbArticle) {
+          // View the added result in the console
+          console.log(dbArticle);
+        })
+        .catch(function(err) {
+          // If an error occurred, log it
+          console.log(err);
+        });
+       
+      });
+  
+      // Send a message to the client
+      res.send("Scrape Complete");
+    });
+});
+
+app.get("/articles", function(req, res) {
+    // Grab every document in the Articles collection
+    db.Article.find({})
+      .then(function(dbArticle) {
+        // If we were able to successfully find Articles, send them back to the client
+        res.json(dbArticle);
+      })
+      .catch(function(err) {
+        // If an error occurred, send it to the client
+        res.json(err);
+      });
+  });
+
+
+
+app.listen(PORT, function() {
+    console.log("App running on port " + PORT + "!");
+  });
