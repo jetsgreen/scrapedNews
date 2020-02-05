612ff644215e3cefb5792de55d48c40e261f6edf
Felix Fernandez <fernandezfelix460.gmail.com>
2020-02-04 20:13:18 -0500

    update

--- server.js
+++ server.js
@@ -1,39 +1,27 @@
 
-
 var express = require("express");
-
+// var logger = require("morgan");
 var mongoose = require("mongoose");
-
-// Our scraping tools
-// Axios is a promised-based http library, similar to jQuery's Ajax method
-// It works on the client and on the server
-var axios = require("axios");
-var cheerio = require("cheerio");
-
+var PORT = process.env.PORT || 3000;
 // Require all models
 var db = require("./models");
-
-var PORT = process.env.PORT || 3000;
-
 // Initialize Express
 var app = express();
-
 // Configure middleware
-
 // Use morgan logger for logging requests
-
+// app.use(logger("dev"));
 // Parse request body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Make public a static folder
 app.use(express.static("public"));
-
 // Connect to the Mongo DB
-// mongoose.connect("mongodb://localhost/mongoHeadlines", { useNewUrlParser: true });
-
-var MONGODB_URI = process.env.MONGOLAB_PURPLE_URI || "mongodb://localhost/mongoHeadlines";
-
-mongoose.connect(MONGODB_URI);
+var databaseUri = "mongodb://localhost/BRscraper1"
+if (process.env.MONGODB_URI) {
+mongoose.connect(process.env.MONGOLAB_PURPLE_URI)
+} else {
+mongoose.connect(databaseUri)
+}
 
 // A GET route for scraping the echoJS website
 app.get("/scrape", function(req, res) {
